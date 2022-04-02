56fca20d524ade2d8f653eb786823dc78bd3eb0e
mjmeilahn <mjmeilahn@gmail.com>
2022-04-02 13:01:21 -0700

    Adds mutations

--- 01-schema.js
+++ 01-schema.js
@@ -1,20 +1,26 @@
 
 const axios = require('axios')
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull, } = graphql
 
 const FactionType = new GraphQLObjectType({
     name: 'Faction',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         description: { type: GraphQLString },
-    }
+        characters: {
+            type: new GraphQLList(CharacterType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/factions/${parentValue.id}/characters`).then(res => res.data)
+            }
+        }
+    })
 })
 
 const CharacterType = new GraphQLObjectType({
     name: 'Character',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -24,7 +30,7 @@ const CharacterType = new GraphQLObjectType({
                 return axios.get(`http://localhost:3000/factions/${parentValue.factionId}`).then(res => res.data)
             }
         }
-    }
+    })
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -47,6 +53,37 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addCharacter: {
+            type: CharacterType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                factionId: { type: GraphQLString }, // OPTIONAL FIELD
+            },
+            resolve(parentValue, args) {
+                const { name, age } = args
+                return axios.post(`http://localhost:3000/characters`, {
+                    name,
+                    age
+                }).then(res => res.data)
+            }
+        },
+        deleteCharacter: {
+            type: CharacterType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, args) {
+                return axios.delete(`http://localhost:3000/characters/${args.id}`).then(res => res.data)
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
